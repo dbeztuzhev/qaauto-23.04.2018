d68bfca1a9f8ec69a9de7e6a380759b11e2f1a28
denis_beztuzhev <epapirua@gmail.com>
2018-05-14 22:07:07 +0300

    Lesson 4

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,38 +3,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.sql.Driver;
 
-public class LinkedinLoginTest {
+import static java.lang.Thread.sleep;
 
-   /* @Test
+public class LinkedinLoginTest {
+    WebDriver webDriver;
+
+    @BeforeMethod
+    public void before() {
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("https://www.linkedin.com/");
+
+    }
+
+   @Test
     public void successfulLoginTest() {
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://www.linkedin.com/");
 
         String actualLoginPageTitle = webDriver.getTitle();
 
-        *//*Assert.assertEquals("a", "b", "Probably 'a' is not equal to 'b'");
-     *//*
+        //*Assert.assertEquals("a", "b", "Probably 'a' is not equal to 'b'");
+
         Assert.assertEquals(webDriver.getTitle(),
                 "LinkedIn: Log In or Sign Up",
                 "Login page Title is wrong");
 
+       LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage();
+       linkedinLoginPage.login("db.hideez#gmail.com","201101");
 
-        WebElement emailField = webDriver.findElement(By.id("login-email"));
-        WebElement passwordField = webDriver.findElement(By.id("login-password"));
-        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
 
-        *//*Assert.assertTrue(signInButton.isDisplayed(), "Sign In button is not Displayed");
+       Assert.assertTrue(signInButton.isDisplayed(), "Sign In button is not Displayed");
 
-        emailField.sendKeys("db.hideez@gmail.com");
-        passwordField.sendKeys("201101");
-        signInButton.click();
-
-        Assert.assertEquals(webDriver.getCurrentUrl(),
+                Assert.assertEquals(webDriver.getCurrentUrl(),
                 "https://www.linkedin.com/feed/",
                 "Home page url is wrong.");
 
@@ -50,10 +56,10 @@ public class LinkedinLoginTest {
 
         Assert.assertTrue(webDriver.getTitle().contains("LinkedIn"),
                  "Home page url is wrong.");
-*//*
 
 
-     *//*WebElement emailField = webDriver.findElement(By.xpath("//*[@id=\"login-email\"]"));
+
+     WebElement emailField = webDriver.findElement(By.xpath("//*[@id=\"login-email\"]"));
         emailField.sendKeys("db.hideez@gmail.com");
 
         WebElement passwordField = ((FirefoxDriver) webDriver).findElementByXPath("//*[@id=\"login-password\"]");
@@ -68,44 +74,40 @@ public class LinkedinLoginTest {
 
         Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "Home page Title is wrong");
 
-        *//**//*String actualString = webDriver.findElement(By.xpath("//*[@id=\"ember4072\"]")).getText();
-        Assert.assertTrue(actualString.contains("Добро пожаловать, Den!"));*//*
+        String actualString = webDriver.findElement(By.xpath("//*[@id=\"ember4072\"]")).getText();
+        Assert.assertTrue(actualString.contains("Добро пожаловать, Den!"));
 
         webDriver.close();
     }
-}*/
+}
 
     @Test
-public void wrongEmailCorrectPasswordTest() {
-    WebDriver webDriver = new FirefoxDriver();
-    webDriver.get("https://www.linkedin.com/");
+    public void wrongEmailCorrectPasswordTest() {
 
-    String actualLoginPageTitle = webDriver.getTitle();
+        String actualLoginPageTitle = webDriver.getTitle();
 
-    Assert.assertEquals(webDriver.getTitle(),
-            "LinkedIn: Log In or Sign Up",
-            "Login page Title is wrong");
+        Assert.assertEquals(webDriver.getTitle(),
+                "LinkedIn: Log In or Sign Up",
+                "Login page Title is wrong");
 
+        WebElement wrongEmailField = webDriver.findElement(By.id("login-email"));
+        wrongEmailField.sendKeys("dbgmail.com");
 
-    WebElement wrongEmailField=webDriver.findElement(By.id("login-email"));
-    wrongEmailField.sendKeys("dbgmail.com");
+        WebElement correctPasswordField = webDriver.findElement(By.id("login-password"));
+        correctPasswordField.sendKeys("201101");
 
-    WebElement correctPasswordField =webDriver.findElement(By.id("login-password"));
-    correctPasswordField.sendKeys("201101");
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
+        signInButton.click();
 
-    WebElement signInButton =webDriver.findElement(By.id("login-submit"));
-    signInButton.click();
-
-    Assert.assertEquals(webDriver.getCurrentUrl(),
-            "https://www.linkedin.com/uas/login-submit",
-            "There were one or more errors in your submission. Please correct the marked fields below.");
-    Assert.assertEquals("Please enter a valid email address.", "Please enter a valid email address.");
-    webDriver.close();}
+        Assert.assertEquals(webDriver.getCurrentUrl(),
+                "https://www.linkedin.com/uas/login-submit",
+                "There were one or more errors in your submission. Please correct the marked fields below.");
+        Assert.assertEquals("Please enter a valid email address.", "Please enter a valid email address.");
+        webDriver.close();
+    }
 
     @Test
     public void correctEmailWrongPasswordTest() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://www.linkedin.com/");
 
         String actualLoginPageTitle = webDriver.getTitle();
 
@@ -114,21 +116,23 @@ public void wrongEmailCorrectPasswordTest() {
                 "Login page Title is wrong");
 
 
-        WebElement wrongEmailField=webDriver.findElement(By.id("login-email"));
+        WebElement wrongEmailField = webDriver.findElement(By.id("login-email"));
         wrongEmailField.sendKeys("db.hideez@gmail.com");
 
-        WebElement correctPasswordField =webDriver.findElement(By.id("login-password"));
+        WebElement correctPasswordField = webDriver.findElement(By.id("login-password"));
         correctPasswordField.sendKeys("222222");
 
-        WebElement signInButton =webDriver.findElement(By.id("login-submit"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
         signInButton.click();
 
         String actualString = webDriver.findElement(By.xpath("//*[@id=\"global-alert-queue\"]")).getText();
-        webDriver.close();}
+        Assert.assertTrue(actualString.contains("Hmm, we don't recognize that email. Please try again."));
+
+        webDriver.close();
+    }
+
     @Test
     public void wrongEmailWrongPasswordTest() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://www.linkedin.com/");
 
         String actualLoginPageTitle = webDriver.getTitle();
 
@@ -137,13 +141,13 @@ public void wrongEmailCorrectPasswordTest() {
                 "Login page Title is wrong");
 
 
-        WebElement wrongEmailField=webDriver.findElement(By.id("login-email"));
+        WebElement wrongEmailField = webDriver.findElement(By.id("login-email"));
         wrongEmailField.sendKeys("hideez@gmail.com");
 
-        WebElement wrongPasswordField =webDriver.findElement(By.id("login-password"));
+        WebElement wrongPasswordField = webDriver.findElement(By.id("login-password"));
         wrongPasswordField.sendKeys("222222");
 
-        WebElement signInButton =webDriver.findElement(By.id("login-submit"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
         signInButton.click();
 
         Assert.assertEquals(webDriver.getCurrentUrl(),
@@ -151,12 +155,12 @@ public void wrongEmailCorrectPasswordTest() {
                 "There were one or more errors in your submission.Please correct the marked fields below.");
 
         String actualString = webDriver.findElement(By.xpath("//*[@id=\"session_password-login-error\"]")).getText();
-        webDriver.close();}
+        Assert.assertTrue(actualString.contains("Hmm, that's not the right password. Please try again or request a new one."));
+        webDriver.close();
+    }
 
     @Test
     public void emptyEmailCorrectPasswordTest() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://www.linkedin.com/");
 
         String actualLoginPageTitle = webDriver.getTitle();
 
@@ -165,23 +169,22 @@ public void wrongEmailCorrectPasswordTest() {
                 "Login page Title is wrong");
 
 
-        WebElement emptyEmailField=webDriver.findElement(By.id("login-email"));
+        WebElement emptyEmailField = webDriver.findElement(By.id("login-email"));
         emptyEmailField.sendKeys("");
 
-        WebElement correctPasswordField =webDriver.findElement(By.id("login-password"));
+        WebElement correctPasswordField = webDriver.findElement(By.id("login-password"));
         correctPasswordField.sendKeys("201101");
 
-        WebElement signInButton =webDriver.findElement(By.id("login-submit"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
         signInButton.click();
 
-        Assert.assertEquals(webDriver.getCurrentUrl(),"https://www.linkedin.com/");
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/");
 
-        webDriver.close();}
+        webDriver.close();
+    }
 
     @Test
     public void correctEmailEmptyPasswordTest() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://www.linkedin.com/");
 
         String actualLoginPageTitle = webDriver.getTitle();
 
@@ -190,17 +193,60 @@ public void wrongEmailCorrectPasswordTest() {
                 "Login page Title is wrong");
 
 
-        WebElement correctEmailField=webDriver.findElement(By.id("login-email"));
+        WebElement correctEmailField = webDriver.findElement(By.id("login-email"));
         correctEmailField.sendKeys("");
 
-        WebElement emptyPasswordField =webDriver.findElement(By.id("login-password"));
+        WebElement emptyPasswordField = webDriver.findElement(By.id("login-password"));
         emptyPasswordField.sendKeys("201101");
 
-        WebElement signInButton =webDriver.findElement(By.id("login-submit"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
         signInButton.click();
 
-        Assert.assertEquals(webDriver.getCurrentUrl(),"https://www.linkedin.com/");
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/");
 
-        webDriver.close();}
+        webDriver.close();
     }
 
+    @Test
+    public void negativeTest() throws InterruptedException {
+
+        String actualLoginPageTitle = webDriver.getTitle();
+
+        Assert.assertEquals(webDriver.getTitle(),
+                "LinkedIn: Log In or Sign Up",
+                "Login page Title is wrong");
+
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage();
+        linkedinLoginPage.login("db.hideez#gmail.com","201101");
+
+        WebElement correctEmailField = webDriver.findElement(By.id("login-email"));
+        WebElement wrongPasswordField = webDriver.findElement(By.id("login-password"));
+        WebElement signInButton = webDriver.findElement(By.id("login-submit"));
+
+        correctEmailField.sendKeys("db.hideez@gmail.com");
+        wrongPasswordField.sendKeys("1");
+        signInButton.click();
+
+        sleep(3000);
+
+        String currentPageUrl = webDriver.getCurrentUrl();
+        String currentPageTitle = webDriver.getTitle();
+
+        Assert.assertEquals(currentPageUrl, "https://www.linkedin.com/uas/login-submit",
+                "Login-Submit page url is wrong");
+        Assert.assertEquals(currentPageTitle, "Sign In to LinkedIn",
+                "Login-Submit page Title is wrong");
+
+
+        WebElement errorMessage = ((FirefoxDriver) webDriver).findElement(By.xpath("//div[@role='alert']"));
+        Assert.assertEquals(errorMessage.getText(),
+                "There were one or more errors in your submission. Please correct the marked fields below.",
+                "Wrong error message text displayed.");
+    }
+
+    @AfterMethod
+    public void after(){
+        webDriver.close();
+    }
+
+}
